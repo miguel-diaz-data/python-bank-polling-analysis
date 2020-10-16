7fc6b2492e138f2d0d38688f3810037064cef14a
Miguel Diaz <71040795+miguel-diaz-data@users.noreply.github.com>
2020-10-16 19:56:54 -0400

    starting to code
    changes csv file name to requested name,
    1st stab at implementing requests

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,16 +1,26 @@
 import os
 import csv
 
+total_months = 0
+delta = 0
+total_changes = 
+
 # create filepath for budget data file and read csv file
 csvpath = os.path.join("Resources", "budget_data.csv")
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
+    csv_header = next(csvfile)
+    print(f"{csv_header}")
+    #checked if first row was iterated through properly as header isn't part of data
+
+    for row in csvreader:
 #The total number of months included in the dataset
 #can be done through looping and counting the number of iterations. or len function if it exists
-
+        total_months += 1
 #The net total amount of "Profit/Losses" over the entire period
+        delta = int(row(1))
 #can be done through reading the second column and converting to int, removing any - signs and adding it all up
 
 #The average of the changes in "Profit/Losses" over the entire period
